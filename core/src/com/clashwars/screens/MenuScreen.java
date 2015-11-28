78e0d26d880b8cc6f6014374758eab01e9bc801d
Nelson Ong <contact@nelsonong.com>
2015-11-28 07:50:58 -0500

    Add background to MenuScreen

--- core/src/com/clashwars/screens/MenuScreen.java
+++ core/src/com/clashwars/screens/MenuScreen.java
@@ -1,6 +1,8 @@
 package com.clashwars.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 
 
 public class MenuScreen implements Screen{
@@ -11,7 +13,10 @@ public class MenuScreen implements Screen{
 
     @Override
     public void render(float delta) {
-
+        // Sets a Color to Fill the Screen with (RGB = 10, 15, 230), Opacity of 1 (100%)
+        Gdx.gl.glClearColor(10/255.0f, 15/255.0f, 230/255.0f, 1f);
+        // Fills the screen with the selected color
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     }
 
     @Override

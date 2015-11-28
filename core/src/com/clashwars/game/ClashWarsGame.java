c0696c23b321f6aabcdd88b2cba5f87399e389e6
Nelson Ong <contact@nelsonong.com>
2015-11-28 07:43:58 -0500

    Set Menu screen

--- core/src/com/clashwars/game/ClashWarsGame.java
+++ core/src/com/clashwars/game/ClashWarsGame.java
@@ -1,20 +1,26 @@
 package com.clashwars.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.clashwars.screens.MenuScreen;
 
-public class ClashWarsGame extends ApplicationAdapter {
+public class ClashWarsGame extends Game {
 	SpriteBatch batch;
 	Texture img;
-	
+	private MenuScreen menu;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
-		Connection c = new Connection();
+		//Connection c = new Connection();
+		menu = new MenuScreen();
+		setScreen(menu);
+
 	}
 
 	@Override
